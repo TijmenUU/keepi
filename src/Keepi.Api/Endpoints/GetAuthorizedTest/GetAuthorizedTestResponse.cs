81b9607c06ff7790525108939667abd425fbe905
Tijmen van Nesselrooij <tijmen_222@hotmail.com>
2025-05-18 15:57:00 +0200

    Delete test endpoints

--- src/Keepi.Api/Endpoints/GetAuthorizedTest/GetAuthorizedTestResponse.cs
+++
@@ -1,6 +0,0 @@
-namespace Keepi.Api.Endpoints.GetAuthorizedTest;
-
-public class GetAuthorizedTestResponse
-{
-  public string Message { get; set; } = string.Empty;
-}
--- src/Keepi.Api/Endpoints/GetTest/GetTestResponse.cs
+++
@@ -1,6 +0,0 @@
-namespace Keepi.Api.Endpoints.GetTest;
-
-public class GetTestResponse
-{
-  public string Message { get; set; } = string.Empty;
-}
