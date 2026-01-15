570f1e3685ce950028e643868ee40d2fe2203d72
Tijmen van Nesselrooij <tijmen_222@hotmail.com>
2026-01-15 19:12:52 +0100

    Make spinner theme compatible

--- src/Keepi.Vue/src/components/KeepiSpinner.vue
+++ src/Keepi.Vue/src/components/KeepiSpinner.vue
@@ -1,11 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useDark } from '@vueuse/core'
+
+const isDark = useDark()
+</script>
 
 <template>
-  <div class="loader"></div>
+  <div v-if="isDark" class="loader-white"></div>
+  <div v-else class="loader-black"></div>
 </template>
 
 <style scoped>
-.loader {
+.loader-white {
   height: 15px;
   aspect-ratio: 4;
   --_g: no-repeat radial-gradient(farthest-side, #eee 90%, #0000);
@@ -15,8 +20,8 @@
   background-size: 25% 100%;
   display: grid;
 }
-.loader:before,
-.loader:after {
+.loader-white:before,
+.loader-white:after {
   content: '';
   height: inherit;
   aspect-ratio: 1;
@@ -27,7 +32,35 @@
   background: #eee;
   animation: l49 1s infinite linear;
 }
-.loader:after {
+.loader-white:after {
+  transform-origin: 200% 50%;
+  --s: -1;
+  animation-delay: -0.5s;
+}
+
+.loader-black {
+  height: 15px;
+  aspect-ratio: 4;
+  --_g: no-repeat radial-gradient(farthest-side, #000 90%, #0000);
+  background:
+    var(--_g) left,
+    var(--_g) right;
+  background-size: 25% 100%;
+  display: grid;
+}
+.loader-black:before,
+.loader-black:after {
+  content: '';
+  height: inherit;
+  aspect-ratio: 1;
+  grid-area: 1/1;
+  margin: auto;
+  border-radius: 50%;
+  transform-origin: -100% 50%;
+  background: #000;
+  animation: l49 1s infinite linear;
+}
+.loader-black:after {
   transform-origin: 200% 50%;
   --s: -1;
   animation-delay: -0.5s;
