8248f51a351eea63b599ae0dabc6701111fe10cd
Tijmen van Nesselrooij <tijmen_222@hotmail.com>
2026-01-15 18:46:12 +0100

    Ignore unsupported data attribute error

--- src/Keepi.Vue/src/components/ui/native-select/NativeSelectOptGroup.vue
+++ src/Keepi.Vue/src/components/ui/native-select/NativeSelectOptGroup.vue
@@ -9,6 +9,7 @@ const props = defineProps<{ class?: HTMLAttributes["class"] }>()
 </script>
 
 <template>
+  <!-- @vue-expect-error Object literal may only specify known properties, and ''data-slot'' does not exist in type 'OptgroupHTMLAttributes & ReservedProps'. -->
   <optgroup data-slot="native-select-optgroup" :class="cn('bg-popover text-popover-foreground', props.class)">
     <slot />
   </optgroup>
