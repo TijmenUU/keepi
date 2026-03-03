a67ce79d298f6e20650165d105578984f7896c98
Tijmen van Nesselrooij <tijmen_222@hotmail.com>
2026-03-03 12:31:45 +0100

    Add instructions for testing publish app locally

--- publish.sh
+++ publish.sh
@@ -2,4 +2,8 @@
 
 rm -r published/
 
+# To run the app locally you can use the following steps:
+# cd published/
+# export ASPNETCORE_URLS=https://localhost:7093/;
+# ./Keepi.Web 
 dotnet publish -c Release src/Keepi.Web/ -o published/
