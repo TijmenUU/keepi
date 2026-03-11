da8f924f1314dae7e338e3a79fe35f04ae1af382
Tijmen van Nesselrooij <tijmen_222@hotmail.com>
2026-03-11 15:59:10 +0100

    Validate TestContextTargetDependency ctor arguments

--- tests/Keepi.Generators/TestContextTargetDependency.cs
+++ tests/Keepi.Generators/TestContextTargetDependency.cs
@@ -1,9 +1,27 @@
+using System;
+
 namespace Keepi.Generators;
 
 internal sealed class TestContextTargetDependency
 {
     public TestContextTargetDependency(string fullName, string shortName)
     {
+        if (string.IsNullOrWhiteSpace(fullName))
+        {
+            throw new ArgumentException(
+                message: "Fullname cannot be empty",
+                paramName: nameof(fullName)
+            );
+        }
+
+        if (string.IsNullOrWhiteSpace(shortName))
+        {
+            throw new ArgumentException(
+                message: "Short name cannot be empty",
+                paramName: nameof(shortName)
+            );
+        }
+
         var isLoose = fullName.StartsWith("Microsoft.Extensions.Logging.ILogger<");
 
         FullName = fullName;
